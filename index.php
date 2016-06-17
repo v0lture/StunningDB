d4676e6834ffaeed6af25b9c046d497b397eb699
jazzandmax <randomdeathbymusic@v0ltureprogramming.com>
2016-06-17 08:17:21 -0600

    Basic structure for index
    Index being the landing for viewing databases and any currently selected one

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+    $wd = getcwd();
+    // to be needed later | require_once $wd."/assets/php/session.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <title>v0ltureDB</title>
+        <meta charset="UTF-8">
+
+        <!-- Bootstrap Rscs -->
+        <link href="assets/bootstrap/bootstrap.min.css" rel="stylesheet">
+        <script src="assets/bootstrap/jquery-2.2.4.min.js"></script>
+        <script src="assets/bootstrap/bootstrap.min.js"></script>
+
+    </head>
+
+    <body>
+
+        
+
+    </body>
+</html>
