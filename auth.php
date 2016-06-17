3be665a6d35021a727321ded24efd36b0c2a2917
jazzandmax <randomdeathbymusic@v0ltureprogramming.com>
2016-06-17 09:08:50 -0600

    Added user switching panel

--- auth.php
+++ auth.php
@@ -94,6 +94,59 @@
                         </div>
 
                     </div>
+
+                <?php elseif($confirm == "switch_user"): ?>
+                    <div class="panel panel-success">
+
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Switching users</h3>
+                        </div>
+
+                        <div class="panel-body">
+
+                            <form class="form-horizontal">
+
+                                <fieldset>
+                                    <legend>Relogin as:</legend>
+                                    <div class="form-group">
+
+                                        <label for="auth_username" class="col-lg-2 control-label">Username</label>
+                                        
+                                        <div class="col-lg-10">
+                                            <input type="text" class="form-control" name="auth_username" id="auth_username" placeholder="" required>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="form-group">
+
+                                        <label for="auth_password" class="col-lg-2 control-label">Password</label>
+                                        
+                                        <div class="col-lg-10">
+                                            <input type="password" class="form-control" name="auth_password" id="auth_password" placeholder="" required>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="form-group">
+                                        
+                                        <div class="col-lg-10 col-lg-offset-2">
+
+                                            <button type="submit" class="btn btn-default">Log Out</button>
+                                            <button type="submit" class="btn btn-success">Log In</button>
+                                        
+                                        </div>
+                                    
+                                    </div>
+
+                                </fieldset>
+
+                            </form>
+
+                        </div>
+
+                    </div>
+                
                 <?php endif; ?>
 
             </div>
