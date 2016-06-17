cfa3a86fa3258ba04bad5182beaa48e73173625a
jazzandmax <randomdeathbymusic@v0ltureprogramming.com>
2016-06-17 14:51:48 -0600

    Added dynamic head and v0lture Design
    Added the v0lture Design resources in compliance with the new scheme for v0lture projects.
    The head section of the pages are now served by one page which helps with keeping changes synced to all pages.

--- auth.php
+++ auth.php
@@ -83,13 +83,7 @@
 
     <head>
 
-        <title>v0ltureDB</title>
-        <meta charset="UTF-8">
-
-        <!-- Bootstrap Rscs -->
-        <link href="assets/bootstrap/bootstrap.min.css" rel="stylesheet">
-        <script src="assets/bootstrap/jquery-2.2.4.min.js"></script>
-        <script src="assets/bootstrap/bootstrap.min.js"></script>
+        <?php require_once "assets/page_rsc/head.php"; ?>
 
     </head>
 
