fd121ce4159a3e14517d72863d946a06d8573695
jazzandmax <randomdeathbymusic@v0ltureprogramming.com>
2016-06-17 09:12:03 -0600

    added switch host panel

--- auth.php
+++ auth.php
@@ -133,7 +133,49 @@
                                         <div class="col-lg-10 col-lg-offset-2">
 
                                             <button type="submit" class="btn btn-default">Log Out</button>
-                                            <button type="submit" class="btn btn-success">Log In</button>
+                                            <button type="submit" class="btn btn-success">Switch</button>
+                                        
+                                        </div>
+                                    
+                                    </div>
+
+                                </fieldset>
+
+                            </form>
+
+                        </div>
+
+                    </div>
+
+                <?php elseif($confirm == "switch_host"): ?>
+                    <div class="panel panel-success">
+
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Switching hosts</h3>
+                        </div>
+
+                        <div class="panel-body">
+
+                            <form class="form-horizontal">
+
+                                <fieldset>
+                                    <legend>Relogin at:</legend>
+                                    <div class="form-group">
+
+                                        <label for="auth_username" class="col-lg-2 control-label">Host</label>
+                                        
+                                        <div class="col-lg-10">
+                                            <input type="text" class="form-control" name="auth_username" id="auth_username" placeholder="" required>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="form-group">
+                                        
+                                        <div class="col-lg-10 col-lg-offset-2">
+
+                                            <button type="submit" class="btn btn-default">Log Out</button>
+                                            <button type="submit" class="btn btn-success">Switch</button>
                                         
                                         </div>
                                     
